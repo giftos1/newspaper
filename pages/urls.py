82e9bff94befee7f66415fbccf3ffd0ef1b5f7b6
giftos1 <giftsydney676@gmail.com>
2024-06-15 00:42:44 +1000

    Complete authentication for users

---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+from pages.views import HomePageView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+]
---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from users.views import SignUpView
+from django.urls import path
+
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+]
